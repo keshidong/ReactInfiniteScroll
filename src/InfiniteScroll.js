a7b57fac6140f933663590f3fa66a0f714b123fb
Shidong Ke <shidong.ke@shopee.com>
2018-08-08 20:08:40 +0800

    init project

---
+++ src/InfiniteScroll.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class InfiniteScroll extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+    )
+        ;
+    }
+}
+
+InfiniteScroll.propTypes = {
+
+};
+InfiniteScroll.defaultProps = {};
+
+export default InfiniteScroll;
